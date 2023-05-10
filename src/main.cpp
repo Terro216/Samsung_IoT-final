b2cd70d6b1889f9e03cbea3b4adc597c95d60076
ilyamed <0ilya@bk.ru>
2023-05-10 16:27:25 +0300

    cp

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,143 @@
 #include <Arduino.h>
-#include "GyverStepper-main/src/GyverStepper.h"
-#include <vector>
+#include "GyverStepper-main/src/StepperCore.h"
+#include "tfmini-master/src/TFMini.h"
+#include <SoftwareSerial.h>
 
-GStepper<STEPPER4WIRE> stepper(1, 5, 3, 4, 2);
+Stepper<STEPPER4WIRE> stepper(5, 3, 4, 2);
 
 class Point
 {
+public:
   int deg;
   int step;
-  Point(int deg, int step)
+  int distance;
+  int strength;
+  Point(int deg = 0, int step = 0, int distance = 0, int strength = 0)
   {
     this->deg = deg;
     this->step = step;
+    this->distance = distance;
+    this->strength = strength;
   };
 };
 
-// 4076
-// steps for round = 2038
-// 1 deg = 5.66111111 steps
+TFMini tfmini;
+
+SoftwareSerial SerialTFMini(9, 8); // The only value that matters here is the first one, Rx
+
+void getTFminiData(int *distance, int *strength)
+{
+  static char i = 0;
+  char j = 0;
+  int checksum = 0;
+  static int rx[9];
+  if (SerialTFMini.available())
+  {
+    rx[i] = SerialTFMini.read();
+    if (rx[0] != 0x59)
+    {
+      i = 0;
+    }
+    else if (i == 1 && rx[1] != 0x59)
+    {
+      i = 0;
+    }
+    else if (i == 8)
+    {
+      for (j = 0; j < 8; j++)
+      {
+        checksum += rx[j];
+      }
+      if (rx[8] == (checksum % 256))
+      {
+        *distance = rx[2] + rx[3] * 256;
+        *strength = rx[4] + rx[5] * 256;
+      }
+      i = 0;
+    }
+    else
+    {
+      i++;
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
-  stepper.setRunMode(KEEP_SPEED);
-  stepper.setMaxSpeed(500); // скорость движения
-  // stepper.setMaxSpeedDeg((int32_t)600);
-  stepper.setSpeed(500);
-  // stepper.setSpeedDeg(1);
+  // while (!Serial)
+  //   ;
+  Serial.println("Initializing...");
+  stepper.dir = 1;                     // или -1
+  stepper.pos = 0;                     // доступ к позиции
+  SerialTFMini.begin(TFMINI_BAUDRATE); // 115200 Initialize the data rate for the SoftwareSerial port
+  tfmini.begin(&SerialTFMini);         // Initialize the TF Mini sensor
 }
 
 bool dir = true;
 int counter = 0;
 int currentDeg = 0;
+const int arrLen = 360;
 uint32_t tmr = 0;
-std::vector<Point> res;
+Point res[arrLen];
+int arrI = 0;
+// Vector<Point> res(storage);
 void loop()
 {
   if (!dir)
     return;
-  stepper.tick();
-  if (millis() - tmr > 12)
+  stepper.step();
+  int newDeg = int(stepper.pos / 5.64) % 360;
+  if (currentDeg != newDeg)
   {
-    tmr = millis();
-    int cur = stepper.getCurrent();
-    if (currentDeg != int(cur / 5.64) % 360)
-    {
-      currentDeg = int(cur / 5.64) % 360;
-      // int currentSteps = stepper.getCurrent();
-      Serial.print(counter);
-      Serial.print("     ");
-      // Serial.print(stepper.getCurrentDeg());
-      Serial.print("     ");
-      Serial.println(currentDeg);
-      res.push_back(Point p(currentDeg, cur));
+    currentDeg = newDeg;
+    // Serial.print(counter);
+    // Serial.print("     ");
+    // Serial.println(currentDeg);
 
-      if (currentDeg == 0)
-      {
-        // stepper.setCurrent(0);
-        counter += 1;
-      }
-      if (counter == 20)
-        dir = false;
+    int distance = 0;
+    int strength = 0;
+
+    getTFminiData(&distance, &strength);
+    while (!distance)
+    {
+      getTFminiData(&distance, &strength);
+      if (distance)
+        // {
+        Serial.println(distance);
+      //   Serial.print("cm\t");
+      //   Serial.print("strength: ");
+      //   Serial.println(strength);
+    }
+    // }
+    // return;
+
+    Point p(currentDeg, stepper.pos, distance, strength);
+    // res[arrI] = p;
+    if (arrI < arrLen)
+      arrI++;
+    else
+      arrI = 0;
+
+    if (currentDeg == 0)
+    {
+      // stepper.setCurrent(0);
+      counter += 1;
+    }
+    if (counter == 1)
+    {
+      dir = false;
       for (Point p : res)
-        Serial.print(p.i); // print out
+      {
+        Serial.println("");
+        Serial.print("deg: ");       // print out
+        Serial.print(p.deg);         // print out
+        Serial.print(" step: ");     // print out
+        Serial.print(p.step);        // print out
+        Serial.print(" distance: "); // print out
+        Serial.print(p.distance);    // print out
+      }
     }
   }
+  delay(5);
 }
-// void loop()
-// {
-//   stepper.tick();
-//   static uint32_t tmr;
-//   if (millis() - tmr >= 30)
-//   {
-//     tmr = millis();
-//     int currentStep = stepper.getCurrent();
-//     // Serial.println(currentStep);
-
-//     // Вычисление текущего градуса поворота
-//     float currentDegree = ((float)currentStep / 64.0) * 360.0; // 64 шага на один оборот
-
-//     while (currentDegree > 360)
-//       currentDegree -= 360;
-//     Serial.println(currentDegree);
-//   }
-// }
